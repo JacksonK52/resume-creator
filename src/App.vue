77cc1dfc5ac03c7686d719b3d94eaa0cc62e5a3a
Jackson52 <jacksonkonjengbam@gmail.com>
2023-10-26 17:57:15 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import { RouterView } from 'vue-router'
+  import Navigation from './components/Navigation.vue';
+</script>
+
+<template>
+  <div class="app">
+    <Navigation />
+    <RouterView />
+  </div>
+  <!-- <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+</template>
+
+<style land="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,300&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', sans-serif;
+    font-weight: 400;
+  }
+
+  .app {
+    min-height: 100vh;
+    position: relative;
+    background-color: #f1f1f1;
+  }
+
+  .container {
+    padding: 0 20px;
+    max-width: 1140px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/views/TemplateView.vue
@@ -0,0 +1,28 @@
+<script>
+  import templatesData from '../data/templates.json';
+
+  export default {
+    data() {
+      return { templates: templatesData }
+    }
+  }
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div v-for="template in templates" :key="template.id">
+      <p>{{ template.title }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
